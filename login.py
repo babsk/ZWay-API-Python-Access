1451d808db18b8950601aa0e15da23abebe741ac
babsk <babs.kershaw@gmail.com>
2016-11-05 08:32:13 +0000

    Uploaded login.py
    Change of IP address for rPi

--- login.py
+++ login.py
@@ -2,14 +2,14 @@ import requests
 import json
 
 # ip address of my raspberry pi
-myip="192.168.1.154"
+myip="192.168.1.180"
 api = "http://"+myip+":8083/ZAutomation/api/v1/"
 login_url = api+"login"
 
 def send ():
     # post a login request
     headers={'accept':'application/json','Content-Type':'application/json'}
-    payload = {'form': 'True','login':'admin','password':'admin','keepme':'false','default_ui':'1'}
+    payload = {'form': 'True','login':'admin','password':'hillview','keepme':'false','default_ui':'1'}
     r = requests.post(login_url,data=json.dumps(payload),headers=headers)
 
     # extract the cookie from the response
