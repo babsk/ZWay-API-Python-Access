6aab482c52e70f3b1c49e41ea9cb345d9b4aa4c5
babsk <babs.kershaw@gmail.com>
2017-03-25 09:17:33 +0000

    Now importing sensitive data from secrets.py

--- login.py
+++ login.py
@@ -1,15 +1,32 @@
+##################################################################
+#
+# LOGIN.PY
+#
+# This script sends a standard login request to the zway server.
+#
+# It can be run standalone, in which case it just prints out the
+# cookie that is returned.
+#
+# When imported into other scripts a call to login.send() returns
+# the cookie which must be stored and used for subsequent Zway API
+# requests.
+#
+# NOTE: before using this script, make sure secrets.py has been
+#       updated with your own raspberry pi / zway details
+#
+##################################################################
+
 import requests
 import json
+import secrets
 
-# ip address of my raspberry pi
-myip="192.168.1.180"
-api = "http://"+myip+":8083/ZAutomation/api/v1/"
+api = "http://"+secrets.myip+":8083/ZAutomation/api/v1/"
 login_url = api+"login"
 
 def send ():
     # post a login request
     headers={'accept':'application/json','Content-Type':'application/json'}
-    payload = {'form': 'True','login':'admin','password':'hillview','keepme':'false','default_ui':'1'}
+    payload = {'form': 'True','login':secrets.username,'password':secrets.password,'keepme':'false','default_ui':'1'}
     r = requests.post(login_url,data=json.dumps(payload),headers=headers)
 
     # extract the cookie from the response
