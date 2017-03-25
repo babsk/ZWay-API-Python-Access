cdf8e60b542e32cdc58d65d7214ec4740878494c
babsk <babs.kershaw@gmail.com>
2017-03-25 09:11:26 +0000

    Create secrets.py
    Added this file to store all the sensitive login data and ip address

---
+++ secrets.py
@@ -0,0 +1,19 @@
+##################################################################
+#
+# SECRETS.PY
+#
+# This is where all the information unique and confidential
+# to my ZWay network live.
+#
+# Change these to string values that are relevant to your system
+# localip has been left as an example.
+#
+##################################################################
+
+# ip addresses of the raspberry pi
+localip="192.168.1.180"
+myip=""
+
+# username and password for zway smarthome
+username=""
+password=""
