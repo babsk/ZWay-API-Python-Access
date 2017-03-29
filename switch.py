422238a1086d949d49d75f29152463a9d45716cb
babsk <babs.kershaw@gmail.com>
2017-03-29 10:23:46 +0100

    Changed to use ZAutomation API

--- switch.py
+++ switch.py
@@ -3,10 +3,11 @@
 # SWITCH.PY
 #
 # This script provides functions to get and set the status of
-# binary power switches in the standard zway API
+# binary power switches in the standard zway automation API
 #
-# It can be run standalone, in which case it logs in, requests a
-# device id from the user, verifies it is a switch, gets the
+# It can be run standalone, in which case it logs in, retrieves
+# a list of switches and displays them, requests a device id from
+# the user, and gets the
 # current status and sets the status to a value provided by the
 # user
 #
@@ -18,42 +19,46 @@
 import requests
 import json
 import login
-import zwaveapi
 
-def get_device_type (cookie,id):
+def get_binary_switches (cookie):
+    switches = []
     headers={'accept':'application/json','Content-Type':'application/json','Cookie':'ZWAYSession='+cookie}
     # now get the data
-    r = requests.get(zwaveapi.api+"Run/zway.devices["+str(id)+"].data.deviceTypeString",data=json.dumps({}),headers=headers)
-    device = r.json()['value']
-    return device
+    r = requests.get(login.api+"devices",data=json.dumps({}),headers=headers)
+    device_list = r.json()['data']['devices']
+    for i in range (0,len(device_list)):
+        if device_list[i]['deviceType'] == 'switchBinary':
+            switches.insert(i,device_list[i])
+    return switches
 
 def get_switch_status (cookie,id):
     headers={'accept':'application/json','Content-Type':'application/json','Cookie':'ZWAYSession='+cookie}
     # now get the data
-    r = requests.get(zwaveapi.api+"Run/zway.devices["+str(id)+"].commandClasses[37].data.level.value",data=json.dumps({}),headers=headers)
-    status = r.json()
+    r = requests.get(login.api+"devices/"+id,data=json.dumps({}),headers=headers)
+    status = r.json()['data']['metrics']['level']
     return status
 
 def set_switch_status (cookie,id,status):
     headers={'accept':'application/json','Content-Type':'application/json','Cookie':'ZWAYSession='+cookie}
     # now get the data
-    r = requests.get(zwaveapi.api+"Run/zway.devices["+str(id)+"].commandClasses[37].Set("+status+")",data=json.dumps({}),headers=headers)
+    r = requests.get(login.api+"devices/"+id+"/command/"+status,data=json.dumps({}),headers=headers)
     return r 
 
 if __name__ == "__main__":
 
-    print ("Switch get/set test")
-    switchid = int(input ("Enter device id of switch: "))
-    
     cookie = login.send()
-    s = get_device_type(cookie,switchid)
+    s = get_binary_switches(cookie)
+    for i in range (0,len(s)):
+        msg = "" + str(s[i]['id'] + "      " + s[i]['metrics']['title'])
+        print (msg)
+    
+    print ("Switch get/set test")
+    switchid = input ("Enter device id of switch: ")
 
-    if (s == 'Binary Power Switch'):
-        status = get_switch_status (cookie,switchid)
-        print ("Status of switch " + str(switchid) + " is " + str(status))
-        status = input("Enter new status of switch(true or false): ")
-        r = set_switch_status(cookie,switchid,status)
-        print (r)
-    else:
-        print ("Device " + str(switchid) + " is not a switch (" + s + ")")
+    status = get_switch_status (cookie,switchid)
+    print ("Status of switch " + str(switchid) + " is " + str(status))
+    status = input("Enter new status of switch(on or off): ")
+    r = set_switch_status(cookie,switchid,status)
+    print (r)
+
 
